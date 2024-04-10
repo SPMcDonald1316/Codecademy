4952044fba749dba55d55d90d8c851071876d8e7
sean <spmcdonald1316@gmail.com>
2024-04-10 17:04:34 -0500

    initial commit

---
+++ Code History/Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Sean McDonald on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
