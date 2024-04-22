cc217ef208b6f86c46c2f701e9a2c496388c36b7
sean <spmcdonald1316@gmail.com>
2024-04-22 17:31:10 -0500

    create user choice func to validate user input

---
+++ swift_programs/RockPaperScissors.swift
@@ -0,0 +1,7 @@
+func getUserChoice(userInput: String) -> String {
+  if userInput == "rock" || userInput == "paper" || userInput == "scissors" {
+    return userInput
+  } else {
+    return "You can only enter rock, paper, or scissors. Try again."
+  }
+}
