ef7bb5f7ab48cb806e104e4cea2642e598d4e486
sean <spmcdonald1316@gmail.com>
2024-04-22 17:38:34 -0500

    create computer choice func, generate random int use switch to return choice based on generated num

--- swift_programs/RockPaperScissors.swift
+++ swift_programs/RockPaperScissors.swift
@@ -5,3 +5,17 @@ func getUserChoice(userInput: String) -> String {
     return "You can only enter rock, paper, or scissors. Try again."
   }
 }
+
+func getComputerChoice() -> String {
+  let randomNumber = Int.random(in: 0...2)
+  switch randomNumber {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    case 2:
+      return "scissors"
+    default:
+      return "Something went wrong"
+  }
+}
