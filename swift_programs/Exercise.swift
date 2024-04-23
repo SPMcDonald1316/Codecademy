32a45694cea34344a8a5e2d345698e038243fa7d
sean <spmcdonald1316@gmail.com>
2024-04-23 16:25:59 -0500

    create regimen struct, create instance of regimen

--- swift_programs/Exercise.swift
+++ swift_programs/Exercise.swift
@@ -14,4 +14,15 @@ struct Exercise {
   }
 }
 
+struct Regimen {
+  var dayOfWeek: String
+  var exercises: [Exercise]
+
+  init(dayOfWeek: String, exercises: [Exercise]) {
+    self.dayOfWeek = dayOfWeek
+    self.exercises = exercises
+  }
+}
+
 var pushUp = Exercise(name: "Push up", muscleGroups: ["Triceps", "Chest", "Shoulders"], reps: 10, sets: 3)
+var mondayRegimen = Regimen(dayOfWeek: "Monday", exercises: [pushUp])
