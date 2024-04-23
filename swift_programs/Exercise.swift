084180142b44c56098ac6cd74b860adfece47a0e
sean <spmcdonald1316@gmail.com>
2024-04-23 16:18:22 -0500

    fix muscle groups type, create push up instance

--- swift_programs/Exercise.swift
+++ swift_programs/Exercise.swift
@@ -1,11 +1,11 @@
 struct Exercise {
   var name: String
-  var muscleGroups: String
+  var muscleGroups: [String]
   var reps: Int
   var sets: Int
   var totalReps: Int
 
-  init(name: String, muscleGroups: String, reps: Int, sets: Int) {
+  init(name: String, muscleGroups: [String], reps: Int, sets: Int) {
     self.name = name
     self.muscleGroups = muscleGroups
     self.reps = reps
@@ -13,3 +13,5 @@ struct Exercise {
     self.totalReps = reps * sets
   }
 }
+
+var pushUp = Exercise(name: "Push up", muscleGroups: ["Triceps", "Chest", "Shoulders"], reps: 10, sets: 3)
