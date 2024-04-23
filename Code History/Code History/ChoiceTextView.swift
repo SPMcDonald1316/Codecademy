16cebb191935fb2efe6cabab12043e073e60df5c
sean <spmcdonald1316@gmail.com>
2024-04-23 17:05:52 -0500

    begin code refactor, create swiftui view

---
+++ Code History/Code History/ChoiceTextView.swift
@@ -0,0 +1,18 @@
+//
+//  ChoiceTextView.swift
+//  Code History
+//
+//  Created by Sean McDonald on 4/23/24.
+//
+
+import SwiftUI
+
+struct ChoiceTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChoiceTextView()
+}
