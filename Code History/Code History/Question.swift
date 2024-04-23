370e4edaf9fb1b0b9ae6c4db2bc402da63b399b2
sean <spmcdonald1316@gmail.com>
2024-04-23 16:51:30 -0500

    create question struct and add properties

---
+++ Code History/Code History/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Sean McDonald on 4/23/24.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+}
