30f30b8d7e9eaa0a142e90e0fd1dd5b1d84cbdc3
sean <spmcdonald1316@gmail.com>
2024-04-25 16:30:57 -0500

    game end flag, create method to update game status

--- Code History/Code History/Models/Game.swift
+++ Code History/Code History/Models/Game.swift
@@ -10,9 +10,18 @@ import Foundation
 struct Game {
     private(set) var currentQuestionIndex = 0
     private(set) var guesses = [Question: Int]()
+    private(set) var isOver = false
     private let questions = Question.allQuestions.shuffled()
     
     mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
         guesses[currentQuestion] = index
+    }
+    
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        } else {
+            isOver = true
+        }
     }
 }
