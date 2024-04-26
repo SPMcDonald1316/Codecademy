ccd8793c22f9d346f28e8e955b91633cbc51534e
sean <spmcdonald1316@gmail.com>
2024-04-26 16:22:03 -0500

    write method to display info and call on instances

--- swift_programs/Pokedex.swift
+++ swift_programs/Pokedex.swift
@@ -4,14 +4,25 @@ class Pokemon {
   var type = [""]
   var ability = [""]
 
-  init(num: Int, name: String, type: [String], ability = [String]) {
+  init(num: Int, name: String, type: [String], ability: [String]) {
     self.num = num
     self.name = name
     self.type = type
     self.ability = ability
+  }
+
+  func displayInfo() {
+    print("Number:  \(num)")
+    print("Name:    \(name)")
+    print("Type:    \(type)")
+    print("Ability: \(ability)\n")
   }
 }
 
 var bulbasaur = Pokemon(num: 1, name: "Bulbasaur", type: ["Grass", "Poison"], ability: ["Overgrow"])
 var charmander = Pokemon(num: 4, name: "Charmander", type: ["Fire"], ability: ["Blaze"])
 var squirtle = Pokemon(num: 7, name: "Squirtle", type: ["Water"], ability: ["Torrent"])
+
+bulbasaur.displayInfo()
+charmander.displayInfo()
+squirtle.displayInfo()
