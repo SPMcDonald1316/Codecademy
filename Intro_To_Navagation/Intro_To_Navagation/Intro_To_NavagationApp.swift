1db4a0c5b4bdb998a80474ce2fd8ed5742b6a352
sean <spmcdonald1316@gmail.com>
2024-04-26 19:45:49 -0500

    initial commit

---
+++ Intro_To_Navagation/Intro_To_Navagation/Intro_To_NavagationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intro_To_NavagationApp.swift
+//  Intro_To_Navagation
+//
+//  Created by Sean McDonald on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Intro_To_NavagationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
