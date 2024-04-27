7a6e87cdd17f07f16508f8da9ce1802ffe497e92
sean <spmcdonald1316@gmail.com>
2024-04-26 20:18:36 -0500

    add navigation view to content view

--- Intro_To_Navagation/Intro_To_Navagation/ContentView.swift
+++ Intro_To_Navagation/Intro_To_Navagation/ContentView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Items in Stock")
-                .font(.title)
-                .padding()
-            Spacer()
-            Button {
-                print("Button tapped")
-            } label: {
-                Text("Shrimp Chips")
+        NavigationView {
+            VStack {
+                Text("Items in Stock")
+                    .font(.title)
+                    .padding()
+                Spacer()
+                Button {
+                    print("Button tapped")
+                } label: {
+                    Text("Shrimp Chips")
+                }
+                Spacer()
             }
-            Spacer()
         }
     }
 }
