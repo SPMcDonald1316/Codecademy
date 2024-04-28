f668638d0f5b01260f4381f1816c513f80d69549
sean <spmcdonald1316@gmail.com>
2024-04-28 16:53:50 -0500

    initial commit

---
+++ AddressBook/AddressBook/AddressBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressBookApp.swift
+//  AddressBook
+//
+//  Created by Sean McDonald on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AddressBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
