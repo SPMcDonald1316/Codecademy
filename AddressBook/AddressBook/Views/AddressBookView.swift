d0eae0c38262a11d6f5e855f108dc2716063b434
sean <spmcdonald1316@gmail.com>
2024-04-28 17:17:56 -0500

    write contacts view from provided code

--- AddressBook/AddressBook/Views/AddressBookView.swift
+++ AddressBook/AddressBook/Views/AddressBookView.swift
@@ -3,14 +3,16 @@
 //  AddressBook
 //
 //  Originally Created by Ben Stone (Codecademy)
-//  All code written and completed by Sean McDonald following iOS Development tutorial on Codecademy.
+//  All code written and completed by Sean McDonald following iOS Development path on Codecademy.
 
 
 import SwiftUI
 
 struct AddressBookView: View {
+    // TODO: STATEOBJECT - Add propery wrapper to viewModel so that it observes changes
     private var viewModel = AddressBookViewModel()
     
+    // TODO: STATE - Add propery wrapper to displayFavoriteCount property so it can be reassigned
     private var displayFavoriteCount = true
     
     var body: some View {
@@ -19,7 +21,7 @@ struct AddressBookView: View {
                 .font(.title)
                 .padding()
             Spacer()
-            ContactsView()
+            ContactsView() // TODO: ENVIRONMENTOBJECT - Pass the viewModel to the ContactsView
             Spacer()
             if displayFavoriteCount {
                 HStack {
@@ -28,6 +30,7 @@ struct AddressBookView: View {
                     Spacer()
                 }
             }
+            // TODO: STATE - Add toggle for displayFavoriteCount
         }
         .background(Color(red: 254/255, green: 240/255, blue: 229/255).ignoresSafeArea())
     }
---
+++ AddressBook/AddressBook/Views/ContactsView.swift
@@ -0,0 +1,39 @@
+//
+//  ContactsView.swift
+//  AddressBook
+//
+//  Created by Ben Stone.
+//  Code written and completed by Sean McDonald following iOS Development path on Codecademy
+//
+
+import SwiftUI
+
+struct ContactsView: View {
+    // TODO: ENVIRONMENTOBJECT - Add view model
+    
+    var body: some View {
+        ForEach(0..<4) {index in // TODO: ENVIRONMENTOBJECT - Use the number of contacts from the view model
+            HStack {
+                VStack {
+                    // TODO: ENVIRONMENTOBJECT - Add the correct name and postal code
+                    Text("Name at index \(index)")
+                    Text("Postal code")
+                        .font(.caption2)
+                }
+                Button (action: {
+                    // TODO: ENVIRONMENTOBJECT - Call the appropriate view model method
+                    print("Star tapped at index: \(index)")
+                }) {
+                    // TODO: ENVIRONMENTOBJECT - Update the star to be filled when the contact is a favorite
+                    Image(systemName: "star")
+                }
+            }
+            .padding()
+            .border(Color.black, width: 1)
+        }
+    }
+}
+
+#Preview {
+    ContactsView() // TODO: ENVIRONMENTOBJECT - Add the view model to the preview
+}
