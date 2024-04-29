58bc111c3be8f6bcb5a53d6575c2b85a2d70809c
sean <spmcdonald1316@gmail.com>
2024-04-29 15:57:32 -0500

    create game view model, create class, subclass of observable object, published property, and internal properties to teack game progress

---
+++ iOS_Development/Code History/Code History/ViewModels/GameViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewModel.swift
+//  Code History
+//
+//  Created by Sean McDonald on 4/29/24.
+//
+
+import SwiftUI
+
+class GameViewModel: ObservableObject {
+    
+    @Published private var game = Game()
+    
+    var currentQuestion: Question {
+        game.currentQuestion
+    }
+    
+    var questionProgressText: String {
+        "\(game.currentQuestionIndex + 1) / \(game.numberOfQuestions)"
+    }
+}
