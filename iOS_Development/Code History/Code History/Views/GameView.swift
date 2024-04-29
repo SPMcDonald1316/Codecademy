72d541bce4cfe4a6fbd223e45a3d82258b8334cd
sean <spmcdonald1316@gmail.com>
2024-04-29 16:07:56 -0500

    integrate the GameViewModel

--- iOS_Development/Code History/Code History/Views/GameView.swift
+++ iOS_Development/Code History/Code History/Views/GameView.swift
@@ -14,26 +14,27 @@ struct GameView: View {
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
         correctAnswerIndex: 2)
     
-    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @StateObject var viewModel = GameViewModel()
     
     var body: some View {
         ZStack {
-            mainColor.ignoresSafeArea()
+            GameColor.main.ignoresSafeArea()
             VStack {
-                Text("1 / 10")
-                    .font(.callout)
-                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                    .padding()
-                Text(question.questionText)
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text(viewModel.questionProgressText)
+                        .font(.callout)
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        .padding()
+                    Text(question.questionText)
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                }
                 Spacer()
                 HStack {
                     ForEach(0..<question.possibleAnswers.count) { answerIndex in
                         Button(action: {
                             print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
                         }, label: {
                             ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                         })
