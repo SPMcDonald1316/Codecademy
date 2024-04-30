93b4eed19154858a828559984aef7b04eae89833
sean <spmcdonald1316@gmail.com>
2024-04-30 16:37:26 -0500

    incorporate ScoreViewModel into ScoreView

--- iOS_Development/Code History/Code History/Views/ScoreView.swift
+++ iOS_Development/Code History/Code History/Views/ScoreView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct ScoreView: View {
+  let viewModel: ScoreViewModel
+  
   var body: some View {
     ZStack {
       GameColor.main.ignoresSafeArea()
       VStack {
         Spacer()
-        Text("Final Score")
+        Text("Final Score:")
           .font(.body)
-        Text("???%")
+        Text("\(viewModel.percentage)%")
           .font(.system(size: 50))
           .bold()
           .padding()
         Spacer()
         VStack {
-          Text("???✅")
-          Text("???❌")
+          Text("\(viewModel.correctGuesses)✅")
+          Text("\(viewModel.incorrectGuesses)❌")
         }.font(.system(size: 30))
         Spacer()
         NavigationLink(
@@ -38,5 +40,5 @@ struct ScoreView: View {
 }
 
 #Preview {
-  ScoreView()
+  ScoreView(viewModel: ScoreViewModel(correctGuesses: 8, incorrectGuesses: 2))
 }
