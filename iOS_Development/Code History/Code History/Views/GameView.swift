1b647a4a029ff9782e3e6b12f831f9fe565810d4
sean <spmcdonald1316@gmail.com>
2024-05-01 10:53:36 -0500

    fix incorrect property placement

--- iOS_Development/Code History/Code History/Views/GameView.swift
+++ iOS_Development/Code History/Code History/Views/GameView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct GameView: View {
-    
   @StateObject var viewModel = GameViewModel()
     
   var body: some View {
@@ -21,15 +20,15 @@ struct GameView: View {
           .padding()
         QuestionView(question: viewModel.currentQuestion)
       }
+      .foregroundColor(.white)
+      .navigationBarHidden(true)
+      .environmentObject(viewModel)
     }
     .background(
       NavigationLink(destination: ScoreView(viewModel: ScoreViewModel(correctGuesses: viewModel.correctGuesses, incorrectGuesses: viewModel.incorrectGuesses)),
                      isActive: .constant(viewModel.gameIsOver),
                      label: { EmptyView() })
     )
-    .foregroundColor(.white)
-    .navigationBarHidden(true)
-    .environmentObject(viewModel)
   }
 }
 
