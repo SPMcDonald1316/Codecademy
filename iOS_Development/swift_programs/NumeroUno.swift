44d6ed21dc0f2bf3d5d012456e433b8217184652
sean <spmcdonald1316@gmail.com>
2024-05-04 11:07:07 -0500

    declare enum with raw values, make iterable

---
+++ iOS_Development/swift_programs/NumeroUno.swift
@@ -0,0 +1,6 @@
+enum ProductType: String, CaseIterable {
+  case aceRepository = "Ace Repository"
+  case dealForcer = "Deal Forcer"
+  case kadencePlanner = "Kadence Planner"
+  case mailCannon = "Mail Cannon"
+}
