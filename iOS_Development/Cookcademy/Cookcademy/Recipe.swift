1dd8fdcd0b27f5b050ef769c3c0f661857ecc812
sean <spmcdonald1316@gmail.com>
2024-05-13 23:38:15 -0500

    create recipe model, add recipe struct

---
+++ iOS_Development/Cookcademy/Cookcademy/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by Sean McDonald on 5/13/24.
+//
+
+import Foundation
+
+struct Recipe {
+  var name: String
+  var description: String
+  var author: String
+  var category: String
+  var ingredients: [String]
+  var directions: [String]
+}
