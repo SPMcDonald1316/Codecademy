3d28cc654456e68b7bcaf4eb38baccd05a377c02
sean <spmcdonald1316@gmail.com>
2024-05-13 23:35:03 -0500

    project initial commit

---
+++ iOS_Development/Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Sean McDonald on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
