814576d21c3abdfa78360588900804aa4ab679f0
sean <spmcdonald1316@gmail.com>
2024-05-15 00:00:48 -0500

    create app color view model and add selected colors to views

--- iOS_Development/Cookcademy/Cookcademy/Views/RecipesListView.swift
+++ iOS_Development/Cookcademy/Cookcademy/Views/RecipesListView.swift
@@ -10,13 +10,17 @@ import SwiftUI
 struct RecipesListView: View {
     @StateObject var recipeData = RecipeData()
     
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         List {
             ForEach(recipes) { recipe in
                 NavigationLink(recipe.mainInformation.name,
-                               destination: RecipeDetailView(recipe: recipe)
-                )
+                               destination: RecipeDetailView(recipe: recipe))
             }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
         .navigationTitle(navigationTitle)
     }
