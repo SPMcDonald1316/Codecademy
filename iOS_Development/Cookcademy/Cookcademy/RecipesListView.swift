b3f71a3f4119fb166ebd06215dd67b12dce28b92
sean <spmcdonald1316@gmail.com>
2024-05-14 22:54:40 -0500

    fix indentation

--- iOS_Development/Cookcademy/Cookcademy/RecipesListView.swift
+++ iOS_Development/Cookcademy/Cookcademy/RecipesListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RecipesListView.swift
 //  Cookcademy
 //
 //  Created by Sean McDonald on 5/13/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RecipesListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RecipesListView()
 }
