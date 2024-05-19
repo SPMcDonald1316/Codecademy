e450323494ba96895980f9c0118ff61c9ccbf0bb
sean <spmcdonald1316@gmail.com>
2024-05-18 23:18:27 -0500

    build form to edit ingredients

---
+++ iOS_Development/Cookcademy/Cookcademy/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -0,0 +1,45 @@
+//
+//  ModifyIngredientView.swift
+//  Cookcademy
+//
+//  Created by Sean McDonald on 5/18/24.
+//
+
+import SwiftUI
+
+struct ModifyIngredientView: View {
+  @State var ingredient: Ingredient
+  
+  var body: some View {
+    VStack {
+      Form {
+        TextField("Ingredient Name", text: $ingredient.name)
+        Stepper(value: $ingredient.quantity, in: 0...100, step: 1.0) {
+          HStack {
+            Text("Quantity:")
+            TextField("Quantity",
+                      value: $ingredient.quantity,
+                      formatter: NumberFormatter())
+            .keyboardType(.numbersAndPunctuation)
+          }
+        }
+        Picker(selection: $ingredient.unit, label:
+                HStack {
+                  Text("Unit")
+                  Spacer()
+                  Text(ingredient.unit.rawValue)
+                }) {
+          ForEach(Ingredient.Unit.allCases, id: \.self) { unit in
+            Text(unit.rawValue)
+          }
+        }
+        .pickerStyle(MenuPickerStyle())
+      }
+    }
+  }
+}
+
+#Preview {
+  @State var emptyIngredient = Ingredient(name: "", quantity: 1.0, unit: .none)
+  return ModifyIngredientView(ingredient: emptyIngredient)
+}
