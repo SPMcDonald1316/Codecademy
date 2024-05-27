9f5e436e6b7ba8bed1782403b16995956fdee752
sean <spmcdonald1316@gmail.com>
2024-05-27 09:19:51 -0500

    add method to recipe list view that returns a binding to a specific recipe

--- iOS_Development/Cookcademy/Cookcademy/Views/ExploreRecipes/RecipesListView.swift
+++ iOS_Development/Cookcademy/Cookcademy/Views/ExploreRecipes/RecipesListView.swift
@@ -71,6 +71,13 @@ extension RecipesListView {
   private var navigationTitle: String {
     "\(category.rawValue) Recipes"
   }
+  
+  func binding(for recipe: Recipe) -> Binding<Recipe> {
+    guard let index = recipeData.index(of: recipe) else {
+      fatalError("Recipe no found")
+    }
+    return $recipeData.recipes[index]
+  }
 }
 
 #Preview {
